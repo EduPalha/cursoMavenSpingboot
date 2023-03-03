76e6b96cec3f1ec3eef4dce2a07f507c4f29b2e6
edupalha <edupalharini@hotmail.com>
2023-03-03 16:41:21 -0300

    adicao diretorio aulaspring

---
+++ aulaspring/src/main/java/com/farmcont/aulasprint/AulasprintApplication.java
@@ -0,0 +1,13 @@
+package com.farmcont.aulasprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulasprintApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(AulasprintApplication.class, args);
+    }
+
+}
---
+++ aulaspring/src/test/java/com/farmcont/aulasprint/AulasprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmcont.aulasprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulasprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
