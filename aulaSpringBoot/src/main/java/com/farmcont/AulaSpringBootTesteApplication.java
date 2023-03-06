bb272b21a33489a7671d234ceb2ecfc2c980df41
edupalha <edupalharini@hotmail.com>
2023-03-06 14:09:31 -0300

    repository, resource e service para categoria no curso springboot

--- aulaSpringBoot/src/main/java/com/farmcont/AulaSpringBootTesteApplication.java
+++ aulaSpringBoot/src/main/java/com/farmcont/AulaSpringBootTesteApplication.java
@@ -1,13 +1,29 @@
 package com.farmcont;
 
+import com.farmcont.domain.Categoria;
+import com.farmcont.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class AulaSpringBootTesteApplication {
+public class AulaSpringBootTesteApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository cr;
 
     public static void main(String[] args) {
         SpringApplication.run(AulaSpringBootTesteApplication.class, args);
     }
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria cat1 = new Categoria("consultório");
+        Categoria cat2 = new Categoria("casa");
 
+        cr.save(cat1);
+        cr.save(cat2);
+    }
 }
